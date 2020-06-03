be67c49f5aabf4aa5f8e572d793c629d1b5f9caa
parinz1234 <parinya.ons@gmail.com>
2020-06-03 14:51:35 +0700

    feat: remove un-use

--- webhook-publisher/index.js
+++
@@ -1,8 +0,0 @@
-const publisher = require('./publisher');
-const makePartnerWebhookAPIfactory = require('./partner-webhook-api-factory');
-const { webhookQueue } = require('../sqs');
-const PartnerWebhookAPIfactory = makePartnerWebhookAPIfactory();
-
-const webhookPublisher = new publisher(PartnerWebhookAPIfactory, webhookQueue);
-
-module.exports = webhookPublisher;
--- webhook/dtos/webhook-context.dto.js
+++
@@ -1,6 +0,0 @@
-module.exports = class WebhookContextDTO {
-  constructor(partnerId, webhookPayload) {
-    this.partnerId = partnerId;
-    this.webhookPayload = webhookPayload;
-  }
-};
